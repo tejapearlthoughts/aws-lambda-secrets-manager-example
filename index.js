b5e04987f7f78de6c6a2a4f6e9288d4aaa372801
tejapearlthoughts <100556645+tejapearlthoughts@users.noreply.github.com>
2022-07-15 15:38:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const SecretsManager = require('./SecretsManager.js');
 
 exports.handler = async (event) => {
     // TODO implement
-    var secretName = '<SecreteName>';
-    var region = '<Region>';
+    var secretName = 'MySecret';
+    var region = 'ap-south-1';
     var apiValue = await SecretsManager.getSecret(secretName, region);
     console.log(apiValue); 
     const response = {
